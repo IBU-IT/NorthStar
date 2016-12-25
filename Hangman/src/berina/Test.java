4e96e900933a52a633f512b043b9c355bbf54081
Qwee <Qwee@QWE>
2016-12-25 02:26:50 +0100

    Returning keyboard class and other fixes

---
+++ Hangman/src/berina/Test.java
@@ -0,0 +1,22 @@
+package berina;
+
+import javax.swing.JFrame;
+
+public class Test {
+
+public static void main(String  args[]){
+			
+			Menu a=new Menu();
+			
+			
+			JFrame f=new JFrame();
+			f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			f.add(a);
+			f.setSize(920,700);
+			f.setVisible(true);
+			f.setTitle("Hang the man Game");
+			f.setResizable(false);
+		}
+	}
+
+
