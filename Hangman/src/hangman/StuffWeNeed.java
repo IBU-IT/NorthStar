c04c201f720417197dd6dc60aea8be1de9961955
Azra Ajdinovic <azra_ajdinovic@hotmail.com>
2016-12-30 13:17:07 +0100

    eo ga sad StuffWeNeed hopefully ispravan

--- Hangman/src/hangman/StuffWeNeed.java
+++ Hangman/src/hangman/StuffWeNeed.java
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 package hangman;
 
 //ova klasa sluzi samo da ove varijable mozemo pozivati i mijenjati u ostalim klasama.
+//ignore this line
 
 public class StuffWeNeed {
 	public static int CAT; 
@@ -16,22 +16,3 @@ public class StuffWeNeed {
 	public static int points;
 	public static int strikes;
 }	
-=======
-package hangman;
-
-//ova klasa sluzi samo da ove varijable mozemo pozivati i mijenjati u ostalim klasama.
-
-public class StuffWeNeed {
-	public static int CAT; 
-	/* 1 - countries
-	 * 2 - cities
-	 * 3 - vegetables
-	 * 4 - movies
-	 * 5 - fruits
-	 * 6 - objects
-	 */
-	public static int score; //nisam jos ove tri varijable stavila u kod
-	public static int points;
-	public static int strikes;
-}	
->>>>>>> masterbh
