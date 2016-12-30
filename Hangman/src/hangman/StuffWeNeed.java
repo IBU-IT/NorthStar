6fbb26ffaea170d86ab3e115722d6e5e4dae404e
Azra Ajdinovic <azra_ajdinovic@hotmail.com>
2016-12-30 12:13:32 +0100

    im not sure what am i doing

---
+++ Hangman/src/hangman/StuffWeNeed.java
@@ -0,0 +1,17 @@
+package hangman;
+
+//ova klasa sluzi samo da ove varijable mozemo pozivati i mijenjati u ostalim klasama.
+
+public class StuffWeNeed {
+	public static int CAT; 
+	/* 1 - countries
+	 * 2 - cities
+	 * 3 - vegetables
+	 * 4 - movies
+	 * 5 - fruits
+	 * 6 - objects
+	 */
+	public static int score; //nisam jos ove tri varijable stavila u kod
+	public static int points;
+	public static int strikes;
+}	
